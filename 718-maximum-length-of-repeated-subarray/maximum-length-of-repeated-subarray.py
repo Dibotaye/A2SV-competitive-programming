c9a598d1e217efd816fcc5c10f1ab076381fa10a
Dibora Taye <diborataye7@gmail.com>
2025-11-11 23:03:56 +0300

    Time: 49 ms (96.59%) | Memory: 18.3 MB (81.17%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,18 +1,46 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        def helper(mid):
+            hash1 = set()
+            hash2 = set()
+            base = 101
+            mod = 2**32
+            a = pow(base, mid, mod)
+            currhash = 0
+            for i in range(n):
+                currhash = currhash *base + nums1[i]
+                if i >= mid:
+                    currhash -= nums1[i-mid]* a
+                currhash %= mod
+
+                if i >= mid-1:
+                    hash1.add(currhash)
+            currhash = 0
+            for i in range(m):
+                currhash = currhash *base + nums2[i]
+                if i >= mid:
+                    currhash -= nums2[i-mid] * a
+                currhash %= mod
+
+                if i >= mid -1:
+                    hash2.add(currhash)
+            return len(hash1.intersection(hash2)) >0
+
+
+        
+
         n= len(nums1)
         m = len(nums2)
-        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        ans = 0
-        
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                    ans= max(ans, dp[i][j])
-
-
-        return ans
+        l =0
+        r = min(n,m)
+        while l< r:
+            mid= (r+l+1)//2
+            if helper(mid):
+                l = mid
+            else:
+                    r = mid-1
+               
+        return l
 
 
 
