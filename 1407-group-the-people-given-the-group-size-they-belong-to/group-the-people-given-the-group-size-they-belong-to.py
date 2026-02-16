5ffdab6c9cf39cd1568d233437cf562712647f45
Dibora Taye <diborataye7@gmail.com>
2026-02-17 00:32:22 +0300

    Time: 3 ms (66.95%) | Memory: 19.5 MB (43.51%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groups = defaultdict(list) 
+        res = []
+
+        for i,j in enumerate(groupSizes):
+            groups[j].append(i)
+            if len(groups[j]) == j:
+                res.append(groups[j])
+                groups[j] = []  
+
+        return res
+            
+
